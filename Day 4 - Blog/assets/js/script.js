7f5769abc5e625964e7fb60d5a0ec97dc78bfc1d
Rakharan <randhikatamar@gmail.com>
2023-01-28 11:16:48 +0700

    Some fix to the script

--- Day 4 - Blog/assets/js/script.js
+++ Day 4 - Blog/assets/js/script.js
@@ -4,15 +4,15 @@ const getData = () => {
 
     // delaclaration variable dom selection
     let title = document.getElementById('projectTitle').value
+
     // date grouping
     let startDate = new Date(document.getElementById('startDate').value)
     let endDate = new Date(document.getElementById('endDate').value)
     let date = `${startDate.getFullYear()} - ${endDate.getFullYear()}`
-
+    let timeInMs = endDate.getTime() - startDate.getTime()
+    let duration = Math.round(timeInMs / (1000 * 60 * 60 * 24))
 
     // Getting all checked technology checkbox
-
-
     let description = document.getElementById('description').value
     let image = document.getElementById('image').files
     let isUsingReact = document.getElementById('react').checked
@@ -20,14 +20,13 @@ const getData = () => {
     let isUsingJs = document.getElementById('javascript').checked
     let isUsingAngular = document.getElementById('angular').checked
 
-
-
     // Convert spesific image to blob object
     image = URL.createObjectURL(image[0])
 
     let dataProject = {
         title,
         date,
+        duration,
         description,
         isUsingReact,
         isUsingAngular,
@@ -37,7 +36,6 @@ const getData = () => {
     }
 
     projects.push(dataProject)
-
     showData()
 }
 
@@ -47,8 +45,6 @@ form.addEventListener('submit', function handleSubmit(event) {
     event.preventDefault()
     form.reset()
 })
-
-
 
 //arrow func to show list of projects
 const showData = () => {
@@ -63,7 +59,7 @@ const showData = () => {
             <div class="projectDetails" id="projectDetails">
                  <div class="detailsHeader">
                      <h4>${project.title} ${project.date}</h4>
-                     <span>durasi: 3 bulan</span>
+                     <span>Durasi pengerjaan: ${isNaN(project.duration) ? `-` : `${project.duration} hari`}</span>
                  </div>
                  <div class="detailsParagraph">
                      ${project.description}
